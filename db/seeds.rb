3bd27ba2c69be07ab07da5bc7cf8771a0cf0fe57
nikitas89 <cvnikita1@gmail.com>
2017-12-26 13:14:43 +0800

    uncommented seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-#for each room. create availaility for each day.
-#ava: id.  available day. quantity mathces room's quantity.
-#booking.
-#
-# admin = Admin.new
-# admin.email = 'admin_manager@hotelbooking.com'
-# admin.password = 'tester123'
-# admin.password_confirmation = 'tester123'
-# admin.save
-#
-# user = User.new
-# user.email = 'user@hotelbooking.com'
-# user.password = 'test123'
-# user.password_confirmation = 'test123'
-# user.save
-#
+
+
+admin = Admin.new
+admin.email = 'admin_manager@hotelbooking.com'
+admin.password = 'tester123'
+admin.password_confirmation = 'tester123'
+admin.save
+
+user = User.new
+user.email = 'user@hotelbooking.com'
+user.password = 'test123'
+user.password_confirmation = 'test123'
+user.save
+
 Room.create(roomType:'Suite Apartment',
   description:'Beach facing view, large room',
   image:'https://pix6.agoda.net/hotelImages/165/165214/165214_17092614410056806986.jpg',
@@ -33,14 +31,14 @@ Room.create(roomType:'Studio Premiere',
   image:'https://pix6.agoda.net/hotelImages/165/165214/165214_17092614060056805901.jpg?s=1024x768',
   quantity:1, price:147.00)
 
-#
-# require 'date'
-# end_date = Date.today + 30
-# date_range = Date.today..end_date
-#
-# date_range.each {|date|
-#   Room.all.each do |room|
-#     room.availabilities.create(available_day: date, quantity: room.quantity)
-#   end
-#
-# }
+
+require 'date'
+end_date = Date.today + 30
+date_range = Date.today..end_date
+
+date_range.each {|date|
+  Room.all.each do |room|
+    room.availabilities.create(available_day: date, quantity: room.quantity)
+  end
+
+}
